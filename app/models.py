424108faf50742e776d3ab630d59fd3948956042
826663910 <826663910@qq.com>
2026-04-25 14:25:15 +0800

    '2222'

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, BigInteger, Text, func
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey,Text, func
 from sqlalchemy.sql.sqltypes import TIMESTAMP   # 导入数据库类型中的日期时间
 from .databases import Base # 模型基类
 
@@ -8,4 +8,12 @@ class Post(Base):
     title = Column(String(200), nullable=False)
     content = Column(Text, nullable=False)
     publish = Column(Boolean, nullable=False)
+    create_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(200), nullable=False, unique=True)
+    password = Column(String(200), nullable=False)
     create_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
